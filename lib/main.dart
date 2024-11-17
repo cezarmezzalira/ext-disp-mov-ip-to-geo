5c5298a69503c2721ee597a8ae09a5f5e7889582
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2024-11-17 13:18:49 -0300

    Adicionada tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:ip_to_geo/map_location.dart';
+import 'package:ip_to_geo/widgets/conn_state.dart';
+import 'package:ip_to_geo/widgets/offline.dart';
+import 'package:ip_to_geo/widgets/online.dart';
+
+const homeRoute = "/home";
+const mapRoute = "/map";
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -11,60 +19,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: homeRoute,
+      routes: {
+        homeRoute: (context) => const MyHomePage(),
+        // mapRoute: (context) => MapLocation()
+      },
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() {
+    return _MyHomePageState();
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  ConnectivityStatus _connectivityStatus = ConnectivityStatus.checking;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("IP -> Geo"),
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: ConnStatus(
+                callback: (status) {
+                  setState(() {
+                    _connectivityStatus = status;
+                  });
+                },
+              ))
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(child: _getCurrentWidget()),
     );
   }
+
+  Widget _getCurrentWidget() => switch (_connectivityStatus) {
+        ConnectivityStatus.checking => const CircularProgressIndicator(),
+        ConnectivityStatus.offline => const Offline(),
+        ConnectivityStatus.online => Online(),
+      };
 }
